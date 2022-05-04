13390bb681a3e2aaa89d0f9fc5389b2a836c4913
Kina999 <katarinazer6@gmail.com>
2022-05-04 23:42:57 +0200

    feat: get all user posts

--- PostMicroservice/src/main/java/com/post/grpc/PostGrpcService.java
+++ PostMicroservice/src/main/java/com/post/grpc/PostGrpcService.java
@@ -1,18 +1,28 @@
 package com.post.grpc;
 
 import com.post.dto.NewPostRequestDTO;
+import com.post.dto.PostsResponseDTO;
+import com.post.mapper.PostMapper;
+import com.post.model.Comment;
 import com.post.model.Post;
 import io.grpc.stub.StreamObserver;
 import net.devh.boot.grpc.server.service.GrpcService;
+import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import proto.*;
 import com.post.service.PostService;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Base64;
+import java.util.List;
+
 @GrpcService
 public class PostGrpcService extends PostGrpcServiceGrpc.PostGrpcServiceImplBase {
 
     private final PostService postService;
+    private static final SimpleDateFormat iso8601Formater = new SimpleDateFormat("dd/MM/yyyy");
 
     @Autowired
     public PostGrpcService(PostService postService) {
@@ -62,5 +72,34 @@ public class PostGrpcService extends PostGrpcServiceGrpc.PostGrpcServiceImplBase
         responseObserver.onNext(commentPostResponseProto);
         responseObserver.onCompleted();
 
+    }
+
+    @Override
+    public void getUserPosts(UserPostsProto request, StreamObserver<UserPostsResponseProto> responseObserver) {
+        List<Post> posts = postService.getPostsFromUser(request.getUserId());
+        List<PostProto> postsProto = new ArrayList<>();
+        for (Post post : posts) {
+            List<CommentProto> commentsProto = new ArrayList<>();
+            for(Comment c : post.getComments()){
+                commentsProto.add(CommentProto.newBuilder().setUserId(c.getUserId()).setText(c.getText()).build());
+            }
+            PostProto postProto = PostProto.newBuilder()
+                                    .setPostId(post.getId())
+                                    .setText(post.getText())
+                                    .setOwnerId(post.getOwnerId())
+                                    .setCreationDate(iso8601Formater.format(post.getCreationDate()))
+                                    .setImage(Base64.getEncoder().encodeToString(post.getImage().getData()))
+                                    .addAllLikes(post.getLikes())
+                                    .addAllComments(commentsProto)
+                                    .addAllDislikes(post.getDislikes())
+                                    .build();
+            postsProto.add(postProto);
+        }
+        UserPostsResponseProto userPostsResponseProto = UserPostsResponseProto.newBuilder()
+                                                        .addAllPosts(postsProto)
+                                                        .build();
+
+        responseObserver.onNext(userPostsResponseProto);
+        responseObserver.onCompleted();
     }
 }
